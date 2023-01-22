41677d03962d7a2b8b09c5e6b14c35031fcb6a79
Benjamin Simmers <bej1380@gmail.com>
2023-01-22 15:48:25 +1000

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+const { Configuration, OpenAIApi } = require("openai");
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.get('/completions/:prompt', async function(req, res, next) {
+  let prompt = req.params.prompt;
+
+  const configuration = new Configuration({
+    apiKey: process.env.APIKEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  
+  const response = await openai.createCompletion({
+    model: "text-davinci-001",
+    prompt: prompt,
+    temperature: 0.4,
+    max_tokens: 64,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  res.send(response.data);
+});
+
+module.exports = router;
